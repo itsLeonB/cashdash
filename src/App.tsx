622526a7209004ef62cd6bcf7413cf3cf91f8d72
itsLeonB <ellionblessan@gmail.com>
2026-02-13 18:52:48 +0700

    feat: add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { Analytics } from "@vercel/analytics/react";
 import { Admin } from "@/components/admin";
 import dataProvider from "./providers/data";
 import authProvider from "./providers/auth";
 import resources from "./resources";
 
 const App = () => (
-  <Admin dataProvider={dataProvider} authProvider={authProvider}>
-    {resources()}
-  </Admin>
+  <>
+    <Admin dataProvider={dataProvider} authProvider={authProvider}>
+      {resources()}
+    </Admin>
+    <Analytics />
+  </>
 );
 
 export default App;

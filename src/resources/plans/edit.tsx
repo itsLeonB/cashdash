ae19c667e122feed5326b32e8cf640ff4f9dfd99
itsLeonB <ellionblessan@gmail.com>
2026-02-25 16:14:34 +0700

    feat: add priority field to plans

--- src/resources/plans/edit.tsx
+++ src/resources/plans/edit.tsx
@@ -1,10 +1,17 @@
-import { Edit, SimpleForm, BooleanInput, TextInput } from "@/components/admin";
-import { required } from "ra-core";
+import {
+  Edit,
+  SimpleForm,
+  BooleanInput,
+  TextInput,
+  NumberInput,
+} from "@/components/admin";
+import { minValue, required } from "ra-core";
 
 const PlanEdit = () => (
   <Edit>
     <SimpleForm>
       <TextInput source="name" validate={required()} />
+      <NumberInput source="priority" validate={(required(), minValue(1))} />
       <BooleanInput source="isActive" label="Is active?" />
     </SimpleForm>
   </Edit>
